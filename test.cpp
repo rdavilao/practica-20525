020059c623031b1634948bb1291d9004b3a8dc69
rdavilao <rdavilao@outlook.com>
2025-04-25 07:27:15 -0500

    Add titulo function

--- test.cpp
+++ test.cpp
@@ -2,7 +2,14 @@
 
 using namespace std;
 
+void titulo();
+
 int main() {
+    titulo();
     cout << "Hola" << endl;
     cout << "Github" << endl;
+}
+
+void titulo() {
+    cout << "ESPE" << endl;
 }
